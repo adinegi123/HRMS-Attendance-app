7a0364431d3298f8be3e733dc617cacb867dd2f7
Tech-PC <abymemp60@gmail.com>
2024-04-23 12:04:29 +0530

    profile page

---
+++ lib/components/custom_text_widget.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class CustomTextStyles {
+  static const TextStyle defaultTextStyle = TextStyle(
+    color: Colors.black,
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+  );
+}
+
+class MyTextWidget extends StatelessWidget {
+  final String text;
+  final TextStyle? style;
+
+  const MyTextWidget({
+    super.key,
+    required this.text,
+    this.style,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 18.0, vertical: 10),
+      child: Text(
+        text,
+        style: style ?? CustomTextStyles.defaultTextStyle,
+      ),
+    );
+  }
+}
+
