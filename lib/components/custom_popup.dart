57f0791c760641361956c69304bd9cf4fe56e4d5
Tech-PC <abymemp60@gmail.com>
2024-04-29 18:01:15 +0530

    logout popup

--- lib/components/custom_popup.dart
+++ lib/components/custom_popup.dart
@@ -1,6 +1,8 @@
 import 'package:attendance_app/components/progress_indicator.dart';
+import 'package:attendance_app/shared%20widgets/logout_popup.dart';
 import 'package:flutter/material.dart';
 
+import '../shared widgets/exit_popup.dart';
 import '../shared widgets/image_picker.dart';
 
 class CustomPopup {
@@ -12,6 +14,20 @@ class CustomPopup {
   }
 
   static late BuildContext _dialogContext;
+
+ /* static showExitPopup({required BuildContext context}) {
+    return showDialog(
+      context: context,
+      builder: (context) => ExitPopup(),
+    );
+  }*/
+
+  static showLogoutPopup({required BuildContext context}) {
+    return showDialog(
+      context: context,
+      builder: (context) => const LogoutPopup(),
+    );
+  }
 
   // static showLogoutPopup({required BuildContext context, String? text}) {
   //   return showDialog(
---
+++ lib/shared widgets/logout_popup.dart
@@ -0,0 +1,24 @@
+import 'package:attendance_app/routes/route_const.dart';
+import 'package:flutter/material.dart';
+
+class LogoutPopup extends StatelessWidget {
+  const LogoutPopup({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Confirm Logout'),
+      content: const Text('Are you sure you want to log out?'),
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.pop(context, false),
+          child: const Text('No'),
+        ),
+        TextButton(
+          onPressed: () => Navigator.pushReplacementNamed(context, Routes.loginPageRoute),
+          child: const Text('Yes'),
+        ),
+      ],
+    );
+  }
+}
