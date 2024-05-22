afd37b1cdbb475a31eb6f5d1e45bc673a91556e4
Tech-PC <abymemp60@gmail.com>
2024-05-22 11:36:51 +0530

    logout popup changes

--- lib/shared/logout_popup.dart
+++ lib/shared/logout_popup.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
 import 'package:attendance_app/Firebase%20methods/firebase_methods.dart';
 import 'package:attendance_app/routes/route_const.dart';
 import 'package:flutter/material.dart';
+
+import '../components/custom_text_widget.dart';
 
 class LogoutPopup extends StatelessWidget {
   const LogoutPopup({super.key});
@@ -8,7 +11,7 @@ class LogoutPopup extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
      return AlertDialog(
-       title: const Text('Confirm Logout'),
+       /*title: const Text('Confirm Logout'),
        content: const Text('Are you sure you want to log out?'),
        actions: [
          TextButton(
@@ -25,7 +28,47 @@ class LogoutPopup extends StatelessWidget {
            },
            child: const Text('Yes'),
          ),
-       ],
-     );
-  }
+       ],*/
+         content: Container(
+           height: 120,
+           child: Column(
+             crossAxisAlignment: CrossAxisAlignment.center,
+             children: [
+               const MyTextWidget(text: "Do you want to logout?"),
+               const SizedBox(height: 30),
+               Row(
+                 children: [
+                   Expanded(
+                     child: ElevatedButton(
+                       onPressed: () async{
+                         await FirebaseMethods.signOut(context);
+                         Navigator.pushNamedAndRemoveUntil(
+                             context,
+                             Routes.loginPageRoute,
+                             ModalRoute.withName(Routes.loginPageRoute));
+                         //exit(0);
+                       },
+                       style: ElevatedButton.styleFrom(
+                           backgroundColor: Colors.white),
+                       child: const Text("Yes",style: TextStyle(color: Colors.black)),
+                     ),
+                   ),
+                   const SizedBox(width: 15),
+                   Expanded(
+                       child: ElevatedButton(
+                         onPressed: () {
+                           Navigator.of(context).pop();
+                         },
+                         style: ElevatedButton.styleFrom(
+                           backgroundColor: Colors.white,
+                         ),
+                         child: const Text("No", style: TextStyle(color: Colors.black)),
+                       ))
+                 ],
+               )
+             ],
+           ),
+         ),
+       );
+     }
 }
